76e6f97f3b205566a290066aec3f4c3606f84c18
Ryohei Shima <shima01dev@gmail.com>
2021-01-19 09:19:35 +0900

    Add UserRepository interface.

---
+++ internal/app/domain/repositories/user.go
@@ -0,0 +1,14 @@
+package repositories
+
+import (
+	"github.com/shimar/go-de-ddd/internal/app/domain/entities"
+	"github.com/shimar/go-de-ddd/internal/app/domain/values"
+)
+
+// UserRepository ユーザーリポジトリー
+type UserRepository interface {
+	Find(values.UserID) (*entities.User, error)
+	FindByName(values.UserName) (*entities.User, error)
+	Save(*entities.User) error
+	Delete(*entities.User) error
+}
