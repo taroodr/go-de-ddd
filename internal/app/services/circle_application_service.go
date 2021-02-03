17bc61101b85fe57805dbbffd1a2633f0659a6a5
Ryohei Shima <shima01dev@gmail.com>
2021-02-03 23:16:02 +0900

    Add Invite function to CircleApplicationService.

--- internal/app/services/circle_application_service.go
+++ internal/app/services/circle_application_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"errors"
 
+	"github.com/shimar/go-de-ddd/internal/app/domain/entities"
 	"github.com/shimar/go-de-ddd/internal/app/domain/factories"
 	"github.com/shimar/go-de-ddd/internal/app/domain/repositories"
 	"github.com/shimar/go-de-ddd/internal/app/domain/services"
@@ -11,15 +12,16 @@ import (
 
 // CircleApplicationService サークルアプリケーションサービス
 type CircleApplicationService struct {
-	circleFactory    factories.CircleFactory
-	circleRepository repositories.CircleRepository
-	circleService    services.CircleService
-	userRepository   repositories.UserRepository
+	circleFactory        factories.CircleFactory
+	circleRepository     repositories.CircleRepository
+	circleService        services.CircleService
+	userRepository       repositories.UserRepository
+	invitationRepository repositories.CircleInvitationRepository
 }
 
 // NewCircleApplicationService サークルアプリケーションサービスを生成する
-func NewCircleApplicationService(cf factories.CircleFactory, cr repositories.CircleRepository, cs services.CircleService, ur repositories.UserRepository) *CircleApplicationService {
-	return &CircleApplicationService{circleFactory: cf, circleRepository: cr, circleService: cs, userRepository: ur}
+func NewCircleApplicationService(cf factories.CircleFactory, cr repositories.CircleRepository, cs services.CircleService, ur repositories.UserRepository, ir repositories.CircleInvitationRepository) *CircleApplicationService {
+	return &CircleApplicationService{circleFactory: cf, circleRepository: cr, circleService: cs, userRepository: ur, invitationRepository: ir}
 }
 
 // Create サークルを登録する
@@ -105,6 +107,63 @@ func (s *CircleApplicationService) Join(cmd *CircleJoinCommand) error {
 	return nil
 }
 
+// Invite サークルに勧誘する
+func (s *CircleApplicationService) Invite(cmd *CircleInviteCommand) error {
+	// NOTE: Tx Begin
+	// 招待した人を取得する
+	fromID, err := values.NewUserID(cmd.FromID())
+	if err != nil {
+		return err
+	}
+	from, err := s.userRepository.Find(*fromID)
+	if err != nil {
+		return err
+	}
+	if from == nil {
+		return errors.New("招待元ユーザーがみつかりません")
+	}
+
+	// 招待された人を取得する
+	toID, err := values.NewUserID(cmd.ToID())
+	if err != nil {
+		return err
+	}
+	to, err := s.userRepository.Find(*toID)
+	if err != nil {
+		return err
+	}
+	if to == nil {
+		return errors.New("招待先ユーザーが見つかりませんでした")
+	}
+
+	// サークルを取得する
+	circleID, err := values.NewCircleID(cmd.CircleID())
+	if err != nil {
+		return err
+	}
+	circle, err := s.circleRepository.Find(circleID)
+	if err != nil {
+		return err
+	}
+	if circle == nil {
+		return errors.New("サークルが見つかりません")
+	}
+
+	// サークルの人数をチェックする
+	if len(circle.Members()) >= 29 {
+		return errors.New("このサークルは満員です")
+	}
+
+	// 招待を登録する
+	invitation := entities.NewCircleInvitation(circle, from, to)
+	if err := s.invitationRepository.Save(invitation); err != nil {
+		return err
+	}
+
+	// NOTE: Tx Commit
+	return nil
+}
+
 // CircleCreateCommand サークル生成コマンド
 type CircleCreateCommand struct {
 	userID string
@@ -135,4 +194,26 @@ func (c *CircleJoinCommand) UserID() string {
 // CircleID サークルIDを取得する
 func (c *CircleJoinCommand) CircleID() string {
 	return c.cirlceID
+}
+
+// CircleInviteCommand サークル勧誘コマンド
+type CircleInviteCommand struct {
+	fromID   string
+	toID     string
+	circleID string
+}
+
+// FromID 勧誘した人のIDを取得する
+func (c *CircleInviteCommand) FromID() string {
+	return c.fromID
+}
+
+// ToID 勧誘された人のIDを取得する
+func (c *CircleInviteCommand) ToID() string {
+	return c.toID
+}
+
+// CirlceID サークルIDを取得する
+func (c *CircleInviteCommand) CircleID() string {
+	return c.circleID
 }
